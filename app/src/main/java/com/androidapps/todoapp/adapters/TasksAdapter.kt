b5ba7fd07e79c735d792250292a7d639a09615b9
Abdallah <abdallahabohamad12@gmail.com>
2024-09-23 01:30:20 +0300

    Finished Delete Task

--- app/src/main/java/com/androidapps/todoapp/adapters/TasksAdapter.kt
+++ app/src/main/java/com/androidapps/todoapp/adapters/TasksAdapter.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.androidapps.todoapp.database.database.TaskDataBase
 import com.androidapps.todoapp.database.model.Task
 import com.androidapps.todoapp.databinding.ItemTaskBinding
 import com.androidapps.todoapp.home.EditTaskActivity
@@ -15,7 +16,7 @@ import java.util.Locale
 class TasksAdapter(private var tasksList: List<Task>) :
     RecyclerView.Adapter<TasksAdapter.TaskViewHolder>() {
 
-    class TaskViewHolder(private val binding: ItemTaskBinding) :
+    class TaskViewHolder(val binding: ItemTaskBinding) :
         RecyclerView.ViewHolder(binding.root) {
         private val dateFormat = SimpleDateFormat("d/M/yyyy", Locale.getDefault())
         fun bind(task: Task) {
@@ -25,12 +26,6 @@ class TasksAdapter(private var tasksList: List<Task>) :
                 dateFormat.format(it)
             }
             binding.idTaskDate.text = formattedDate ?: "No Date"
-            binding.idEditTaskButton.setOnClickListener {
-                val context = itemView.context
-                val intent = Intent(context, EditTaskActivity::class.java)
-                intent.putExtra(Constance.TASK_KEY, task)
-                context.startActivity(intent)
-            }
         }
     }
 
@@ -46,11 +41,35 @@ class TasksAdapter(private var tasksList: List<Task>) :
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
         val task = tasksList[position]
         holder.bind(task)
+        editButton(holder, task)
+        deleteButton(holder, task)
+    }
+
+    private fun editButton(holder: TaskViewHolder, task: Task) {
+        holder.binding.idEditTaskButton.setOnClickListener {
+            val context = holder.binding.root.context
+            val intent = Intent(context, EditTaskActivity::class.java)
+            intent.putExtra(Constance.TASK_KEY, task)
+            context.startActivity(intent)
+        }
+    }
+
+    private fun deleteButton(holder: TaskViewHolder, task: Task) {
+        holder.binding.idDeleteIcon.setOnClickListener {
+            deleteTask(task, holder)
+        }
     }
 
     @SuppressLint("NotifyDataSetChanged")
     fun updateList(tasks: List<Task>) {
         tasksList = tasks
         notifyDataSetChanged()
+    }
+
+    private fun deleteTask(task: Task, holder: TaskViewHolder) {
+        val context = holder.binding.root.context
+        val taskDao = TaskDataBase.getInstance(context).getTaskDao()
+        taskDao.deleteTask(task)
+        updateList(taskDao.getAllTasks())
     }
 }
